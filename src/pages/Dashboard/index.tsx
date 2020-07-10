9be029c9d4155c402fc2e3b41b6cf68d1e853f19
Qnedy <altmaisefequatro@gmail.com>
2020-07-09 23:08:53 -0300

    html dashboard ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github explorer" />
+      <Title>Explore repositórios no Github.</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25750884?s=460&u=bc904c520bd7674597315b49975bfed4df26f3fd&v=4"
+            alt="kennedy"
+          />
+          <div>
+            <strong>qnedy/teste</strong>
+            <p>teste de descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25750884?s=460&u=bc904c520bd7674597315b49975bfed4df26f3fd&v=4"
+            alt="kennedy"
+          />
+          <div>
+            <strong>qnedy/teste</strong>
+            <p>teste de descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25750884?s=460&u=bc904c520bd7674597315b49975bfed4df26f3fd&v=4"
+            alt="kennedy"
+          />
+          <div>
+            <strong>qnedy/teste</strong>
+            <p>teste de descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
